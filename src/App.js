9a4c19a54c38bffa72ed10184c39ecb1df07c4a5
ohnas <ohnas12@gmail.com>
2022-11-19 13:12:11 +0900

    3.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import {useState, useEffect} from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/movie" element={<Detail />}/>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
